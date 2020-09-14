4fa344821a68046fd11119ec0f1119919daa17f2
imgncreative <imgncreative@gmail.com>
2020-09-14 21:38:02 +0700

    update page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 // sticky menu
 $(document).ready(function(){
-  var num = 200; 
+  var num = 800; 
 
   $(window).bind('scroll', function () {
       if ($(window).scrollTop() > num) {
@@ -11,6 +11,130 @@ $(document).ready(function(){
       }
   });
 });
+
+// scrolling
+
+$(document).ready(function(){
+  $(".pagescroll").on("click", function (event) {
+      event.preventDefault();
+      $("html,body").animate({
+          scrollTop: $(this.hash).offset().top
+      }, 800);
+  });
+});
+
+// service - links with video content active start
+$(document).ready(function () {
+  $('.services-nav-links li').on('click', function (e) {
+    e.preventDefault()
+    $(this).closest('ul').find('li.active').removeClass('active')
+    $(this).addClass('active')
+    var url_content = $(this).find('.nav-link').html()
+    console.log(url_content);
+
+    if (url_content == 'Cargo Handling') {
+      cargoHandlingContent()
+    } else if(url_content == 'Logistic Information System') {
+      logisticContent()
+    } else if(url_content == 'Storage and Supply Chain'){
+      storageContent()
+    } else if(url_content == 'Sea and Inland Transportation'){
+      transportContent()
+    } else if(url_content == 'Customs Services'){
+      customServiceContent()
+    }
+
+    $('video').each(function () {
+      var $parentSection = $(this).parents('.video-section')
+      var $self = $(this)
+      var $use = $parentSection.find('use')
+      var $playBtn = $parentSection.find('.play-icon')
+      var parentDOM = $self.parent().parent()
+      $use.attr('xlink:href', '#play')
+      $playBtn.css('margin-left', '5px')
+      parentDOM.find('.video-play-text').html('PLAY')
+      this.pause()
+      this.currentTime = 0
+    })
+
+    function cargoHandlingContent () {
+      //hide others
+      $('.logistic-system-block').addClass('hide').fadeOut()
+      $('#logistic-system').addClass('hide').fadeOut()
+      $('.storage-block').addClass('hide').fadeOut()
+      $('#storage').addClass('hide').fadeOut()
+      $('.transport-block').addClass('hide').fadeOut()
+      $('#transport').addClass('hide').fadeOut()
+      $('.custom-service-block').addClass('hide').fadeOut()
+      $('#custom-service').addClass('hide').fadeOut()
+
+      //show this
+      $('.cargo-handling-block').removeClass('hide').fadeIn()
+      $('#cargo').removeClass('hide').fadeIn()
+    }
+    function logisticContent () {
+      //hide others
+      $('.cargo-handling-block').addClass('hide').fadeOut()
+      $('#cargo').addClass('hide').fadeOut()
+      $('.storage-block').addClass('hide').fadeOut()
+      $('#storage').addClass('hide').fadeOut()
+      $('.transport-block').addClass('hide').fadeOut()
+      $('#transport').addClass('hide').fadeOut()
+      $('.custom-service-block').addClass('hide').fadeOut()
+      $('#custom-service').addClass('hide').fadeOut()
+
+      //show this
+      $('.logistic-system-block').removeClass('hide').fadeIn()
+      $('#logistic-system').removeClass('hide').fadeIn()
+    }
+    function storageContent () {
+      //hide others
+      $('.cargo-handling-block').addClass('hide').fadeOut()
+      $('#cargo').addClass('hide').fadeOut()
+      $('.logistic-system-block').addClass('hide').fadeOut()
+      $('#logistic-system').addClass('hide').fadeOut()
+      $('.transport-block').addClass('hide').fadeOut()
+      $('#transport').addClass('hide').fadeOut()
+      $('.custom-service-block').addClass('hide').fadeOut()
+      $('#custom-service').addClass('hide').fadeOut()
+
+      //show this 
+      $('.storage-block').removeClass('hide').fadeIn()
+      $('#storage').removeClass('hide').fadeIn()
+    }
+    function transportContent () {
+      //hide others
+      $('.cargo-handling-block').addClass('hide').fadeOut()
+      $('#cargo').addClass('hide').fadeOut()
+      $('.logistic-system-block').addClass('hide').fadeOut()
+      $('#logistic-system').addClass('hide').fadeOut()
+      $('.storage-block').addClass('hide').fadeOut()
+      $('#storage').addClass('hide').fadeOut()
+      $('.custom-service-block').addClass('hide').fadeOut()
+      $('#custom-service').addClass('hide').fadeOut()
+
+      //show this 
+      $('.transport-block').removeClass('hide').fadeIn()
+      $('#transport').removeClass('hide').fadeIn()
+    }
+    function customServiceContent () {
+      //hide others
+      $('.cargo-handling-block').addClass('hide').fadeOut()
+      $('#cargo').addClass('hide').fadeOut()
+      $('.logistic-system-block').addClass('hide').fadeOut()
+      $('#logistic-system').addClass('hide').fadeOut()
+      $('.storage-block').addClass('hide').fadeOut()
+      $('#storage').addClass('hide').fadeOut()
+      $('.transport-block').addClass('hide').fadeOut()
+      $('#transport').addClass('hide').fadeOut()
+
+      //show this 
+      $('.custom-service-block').removeClass('hide').fadeIn()
+      $('#custom-service').removeClass('hide').fadeIn()
+    }
+  })
+})
+// service - links with video content active END
 
 // video-play/pause-action start
   $(document).ready(function () {
@@ -41,13 +165,13 @@ $(document).ready(function(){
         $video.trigger('play')
         $playBtn.css('margin-left', '0')
         $use.attr('xlink:href', '#pause')
-        $('#video-trig').attr('class', 'fas fa-pause')
+        $('.play-icon').find('#video-trig').attr('class', 'fas fa-pause')
         parentDOM.find('.video-play-text').html('PAUSE')
       } else {
         $video.trigger('pause')
         $playBtn.css('margin-left', '5px')
         $use.attr('xlink:href', '#play')
-        $('#video-trig').attr('class', 'fas fa-play')
+        $('.play-icon').find('#video-trig').attr('class', 'fas fa-play')
         parentDOM.find('.video-play-text').html('PLAY')
       }
 
@@ -166,5 +290,4 @@ var wow = new WOW({
     offset:100,   
 });
 wow.init();
-
 
